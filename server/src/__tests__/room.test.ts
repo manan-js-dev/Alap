1c3f0495e0099cb0d1689b9232ea1f1e3302ca93
Manan Patel <manan.js.dev@gmail.com>
2026-07-05 21:06:37 +0530

    ci: fix env variables for Jest tests in GitHub Actions

--- server/src/__tests__/room.test.ts
+++ server/src/__tests__/room.test.ts
@@ -1,8 +1,6 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
 
 import authRoutes from '../routes/auth.routes';
 import roomRoutes from '../routes/room.routes';
